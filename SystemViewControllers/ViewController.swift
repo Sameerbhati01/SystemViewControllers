24406407a13d3b52b63be513613b7f01070f32a4
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-17 11:38:35 +0530

    first

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -6,14 +6,96 @@
 //
 
 import UIKit
+import SafariServices
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image=imageView.image else {return}
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView=sender
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        if let url = URL(string: "https://www.apple.com"){
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose an image source", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(cameraAction)
+        }
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in imagePicker.sourceType = .photoLibrary
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        
+        
+        
+        alertController.addAction(cancelAction)
+        alertController.popoverPresentationController?.sourceView=sender
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {return}
+        
+        imageView.image = selectedImage
+        dismiss(animated: true,completion: nil)
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        
+        guard MFMailComposeViewController.canSendMail() else {
+            print("Cannot send Mail")
+            return
+        }
+        
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.mailComposeDelegate = self
+        mailComposer.setToRecipients(["bhati.sameer987.com"])
+        mailComposer.setSubject("Subject")
+        mailComposer.setMessageBody("Message", isHTML: false)
+        
+        if let image = imageView.image, let jpegData = image.jpegData(compressionQuality: 0.9){
+            mailComposer.addAttachmentData(jpegData, mimeType: "image/jpegData", fileName: "photo.jpg")
+        }
+        
+        present(mailComposer, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: (any Error)?) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
